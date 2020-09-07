af761e94094c94ecd36c64e3643deda46f004c10
¨David¨ <davidrr261096@hotmail.com>
2020-09-07 00:29:52 -0400

    deleting some dependencies and refactoring home screen

--- app/component/index.js
+++ app/component/index.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ActivityIndicator from 'react-activity-indicator';
+
+import getPhraseList from '../utils';
 
 export default function App() {
+    const [phraseList, setPhraseList] = useState([]);
+
+    if (phraseList.length === 0) {
+        getPhraseList(setPhraseList);
+    }
+
     return (
         <>
-            <h1>This should changed when I change it</h1>
+            {phraseList.length > 0 ? (
+                <ul>
+                    {phraseList.map(({ text, author }, index) => (
+                        <div key={`${author}-${index}`}>
+                            <h4>{text}</h4>
+                            <span>{author}</span>
+                        </div>
+                    ))}
+                </ul>
+            ) : (
+                <ActivityIndicator
+                    number={4}
+                    diameter={40}
+                    borderWidth={5}
+                    duration={300}
+                    activeColor="#66D9EF"
+                    borderColor="white"
+                    borderRadius="50%"
+                />
+            )}
         </>
     );
 }
--- app/utils/index.js
+++ app/utils/index.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
-export default async () => {
-    try {
-        return await axios.get('https://type.fit/api/quotes');
-    } catch (err) {
-        console.log('Something went wrong ', err);
-    }
-};
+export default function getPhraseList(setPhrasesList) {
+    axios
+        .get('https://type.fit/api/quotes')
+        .then(({ data }) => setPhrasesList(data))
+        .catch((err) => console.log('Something went wrong ', err));
+}
