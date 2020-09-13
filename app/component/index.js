1587d27970d784fd9eb529fa388fd3436bb0ec9c
¨David¨ <davidrr261096@hotmail.com>
2020-09-13 14:32:29 -0400

    working with styles in the front-end

--- app/component/index.js
+++ app/component/index.js
@@ -2,9 +2,12 @@ import React, { useState } from 'react';
 import ActivityIndicator from 'react-activity-indicator';
 
 import getPhraseList from '../utils';
+// import './style.css';
 
 export default function App() {
     const [phraseList, setPhraseList] = useState([]);
+    // const [logIn, setlogIn] = useState(false);
+    // const [signIn, setSignIn] = useState(false);
 
     if (phraseList.length === 0) {
         getPhraseList(setPhraseList);
@@ -12,10 +15,15 @@ export default function App() {
 
     return (
         <>
+            <button className={'app-signin'}>Sign Up</button>
+
             {phraseList.length > 0 ? (
-                <ul>
+                <ul className={'app-list'}>
                     {phraseList.map(({ text, author }, index) => (
-                        <div key={`${author}-${index}`}>
+                        <div
+                            className={'app-list-item'}
+                            key={`${author}-${index}`}
+                        >
                             <h4>{text}</h4>
                             <span>{author}</span>
                         </div>
@@ -23,7 +31,7 @@ export default function App() {
                 </ul>
             ) : (
                 <ActivityIndicator
-                    number={4}
+                    number={3}
                     diameter={40}
                     borderWidth={5}
                     duration={300}
